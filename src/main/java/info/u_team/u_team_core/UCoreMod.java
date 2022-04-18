af900e1c86914108fe8c0ffbaf70e05b4fb16145
HyCraftHD <hycrafthd@live.de>
2022-04-18 16:15:08 +0200

    Remove dependency on fabric api

--- src/main/java/info/u_team/u_team_core/UCoreMod.java
+++ src/main/java/info/u_team/u_team_core/UCoreMod.java
@@ -1,20 +1,17 @@
 package info.u_team.u_team_core;
 
-import com.mojang.logging.LogUtils;
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.tinyremapper.extension.mixin.common.data.AnnotationElement;
-import org.apache.commons.lang3.AnnotationUtils;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.spongepowered.asm.util.Annotations;
+
+import com.mojang.logging.LogUtils;
+
+import net.fabricmc.api.ModInitializer;
 
 public class UCoreMod implements ModInitializer {
-
+	
 	public static final String MODID = "uteamcore";
 	public static final Logger LOGGER = LogUtils.getLogger();
-
+	
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
 	}
 }
